5b3d5567640f12c87a15fed9850b56fc64b570c4
vishnu-madle <vishnu.madle@stryv.ai>
2024-08-09 15:27:05 +0530

    testing function deployment __init__.py

--- AddDetails/__init__.py
+++ AddDetails/__init__.py
@@ -5,7 +5,7 @@ import os
 import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a request. test')
 
     try:
         req_body = req.get_json()
